1ecf01c5b7135099f79c774e1277a19faa1cb900
Hideo Amezawa <hamezawa3@gmail.com>
2023-12-06 15:47:14 +0900

    add signup function

--- accounts/views.py
+++ accounts/views.py
@@ -5,5 +5,5 @@ from django.views.generic import CreateView
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("list")  # 後でloginに変更
+    success_url = reverse_lazy("login")  # loginは後で実装
     template_name = "accounts/signup.html"
