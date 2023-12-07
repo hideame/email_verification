0cf1d716124e58dd98ad37cf09d760e3170db75c
Hideo Amezawa <hamezawa3@gmail.com>
2023-12-07 18:35:07 +0900

    add login/logout function

--- accounts/views.py
+++ accounts/views.py
@@ -9,5 +9,5 @@ class SignUpView(CreateView):
     # form_class = UserCreationForm
     form_class = SignUpForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("accounts:signup")  # login画面実装するまでの暫定
-    # success_url = reverse_lazy("login")
+    # success_url = reverse_lazy("accounts:signup")  # login画面実装するまでの暫定
+    success_url = reverse_lazy("login")
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("registration/", include("django.contrib.auth.urls")),
 ]
